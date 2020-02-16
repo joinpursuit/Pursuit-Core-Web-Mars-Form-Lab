441c0c1c4f3cc9de039ec0fba05db60ea9de7bb1
John Jones Jr <jayjohnjones@gmail.com>
2020-02-16 15:18:38 -0500

    populated the select button of countries

--- form_lab/src/component/Form.js
+++ form_lab/src/component/Form.js
@@ -1,15 +1,27 @@
 import React from "react"
+import countries from "./countries"
+
+
 
 class Form extends React.Component {
-
+ 
+   populateSelect = () => {
+    return countries.map(country =>{
+     return <option value = {country.name} key = {country.code} >{country.name}</option>
+    })
+    
+  }
+  
 
   render(){
+  
+    // console.log(countries)
           return(
-          
            <div>
             <h1>Mission to Mars Registration Form.</h1>
             
         <form>
+        
         <label>What is your name?
         <input type = "text"/>
         </label>
@@ -20,6 +32,16 @@ class Form extends React.Component {
        value="2018-07-22"
        min="1990-01-01" max="2020-12-31"/>
        </label>
+       <br/>
+
+       
+       <label>
+       What is your country of origin?
+        <select >
+        <option disabled>Select a country</option>
+        {this.populateSelect()}
+        </select>
+        </label>
        <br/>
        
        <label>
@@ -37,13 +59,17 @@ class Form extends React.Component {
         <input type = "text"/>
       </label>
 
+      <button>Submit</button>
+      
        </form>
       </div>
 
 
-          )
+)
+
 }
 }
+
 
 
 export default Form;
