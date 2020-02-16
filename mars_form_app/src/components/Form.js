13e0e8f80b3350f32174dce9c90ead38fcf4d074
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-02-16 12:16:43 -0500

    complete step 1

---
+++ mars_form_app/src/components/Form.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Form extends Component{
+    state = {
+        
+    };
+    render(){
+        return(
+            <div>
+                <form>
+                    <h1>
+                        Mission to Mars Registration Form
+                    </h1>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Form;
