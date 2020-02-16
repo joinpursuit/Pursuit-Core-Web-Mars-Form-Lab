293fecb48549121c1267376ac9236a575b7569a5
maria liza <mariamartinez@pursuit.org>
2020-02-16 15:35:12 -0500

    add dietary needs select

--- mars-mission/src/Conponents/forms.js
+++ mars-mission/src/Conponents/forms.js
@@ -1,58 +1,67 @@
 import React from "react";
-import Select from 'react-select';
-import countriesList from "./countries list/countries"
+import { countriesList } from "./countriesList/countries";
+import { diets } from "./Dietary/diets";
 
-//Creates options for select, however not using
+class Form extends React.Component {
+  state = {
+    fullName: "",
+    date: {},
+    countries: {},
+    diet: {}
+  };
 
-// const months = [
-//     { value: 'January', label: 'January' },
-//     { value: 'Feburary', label: 'Feburary' },
-//     { value: 'March', label: 'March' },
-//     { value: 'April', label: 'April' },
-//     { value: 'May', label: 'May' },
-//     { value: 'June', label: 'June' },
-//     { value: 'July', label: 'July' },
-//     { value: 'August', label: 'August' },
-//     { value: 'September', label: 'September' },
-//     { value: 'October', label: 'October' },
-//     { value: 'November', label: 'November' },
-//     { value: 'December', label: 'December' },
-//   ];
+  handleChange = e => {
+    debugger;
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-class Form extends React.Component{
-    state={
-        fullName: "",
-        date:{},
-        countries:null
-    }
+  populateDiets = () => {
+    return diets.map(diet => {
+      return <option value={diet}>{diet}</option>;
+    });
+  };
 
-    handleChange = (e) => {
-        debugger
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  populateCountries = () => {
+    return countriesList.map(countries => {
+      return <option value={countries.code}>{countries.name}</option>;
+    });
+  };
 
-    handleCountries = countries => {
-        this.setState(
-          { countries },
-          () => console.log(`Option selected:`, this.state.countries)
-        );
-      };
-
-
-    render(){
-        const {fullName, date, countries} = this.state;
-        return(
-        <div>
-            <form>
-               <input type="text" value={fullName} placeholder="Enter Full Name" name="fullName" onChange={this.handleChange}/>
-               <input type="date" value={date} name="date" onChange={this.handleChange}/>
-               <Select value={countries} onChange={this.handleCountries} options={countriesList}/>
-            </form>
-        </div>
-        ) 
-    }
+  render() {
+    const { fullName, date, countries, diet } = this.state;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            value={fullName}
+            placeholder="Enter Full Name"
+            name="fullName"
+            onChange={this.handleChange}
+          />
+          <input
+            type="date"
+            value={date}
+            name="date"
+            onChange={this.handleChange}
+            xs
+          />
+          <select
+            value={countries}
+            name="countries"
+            onChange={this.handleChange}
+          >
+            {this.populateCountries()}
+          </select>
+          <select value={diet} name="diet" onChange={this.handleChange}>
+            {this.populateDiets()}
+          </select>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
