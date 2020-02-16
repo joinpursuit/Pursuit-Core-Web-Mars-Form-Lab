be403e6a54cb0aea538155f4278cfbe70b9f32a5
Phillip <phil.awich@gmail.com>
2020-02-16 13:01:58 -0500

    Create date feature

--- mars-registration-form/src/Components/Form.js
+++ mars-registration-form/src/Components/Form.js
@@ -1,10 +1,31 @@
 import React from "react";
 
 class Form extends React.Component{
+
+state = {firstName: "", lastName: "", date: ""}
+handleChange = (event) => {
+    this.setState({
+        [event.target.name]: event.target.value
+    })
+} 
+
+
 render() {
-    return(<div>
-        Hello 
-    </div>)
+    console.log(this.state)
+    return(
+        <>
+         <h1>Mission to Mars Registration From</h1>
+            <div>
+                <form>
+                    <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name"/>
+                    <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder="Last Name"/>
+                    <br/>
+                    <label for="start">Start date:</label>
+                    <input type="date" id="start" name="trip-start" value={this.state.date} min="1900-01-01" max="2020-02-15"/>
+                </form>
+            </div>
+    </>
+    ) 
 }
 
 
