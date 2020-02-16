ab8c9f993bbb7c14ebcb2d02dc03e3b768380afd
Kevin Brutus <kevinbrutus@pursuit.org>
2020-02-16 16:09:22 -0500

    complete listing countries, dietary preference, and question input

--- mars-registration-form/src/Components/Form.js
+++ mars-registration-form/src/Components/Form.js
@@ -1,8 +1,16 @@
 import React from "react";
+import listOfCountries from "./listOfCountries";
 
 class Form extends React.Component{
 
-state = {firstName: "", lastName: "", dateOfBirth: ""}
+state = {
+    firstName: "", 
+    lastName: "", 
+    dateOfBirth: "", 
+    country: "", 
+    dietaryOptions: "",
+    question: ""
+}
 handleChange = (event) => {
     this.setState({
         [event.target.name]: event.target.value
@@ -16,16 +24,42 @@ render() {
          <h1>Mission to Mars Registration From</h1>
             <div>
                 <form>
+                <label>
+                    First Name:
                     <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} placeholder="First Name"/>
+                </label>
+                <br/>
+                <label>
+                    Last Name:
                     <input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} placeholder="Last Name"/>
+                </label>
                     <br/>
                     <label for="start">
                     Start date:
                     <input type="date" id="start" name="dateOfBirth" value={this.state.dateOfBirth} min="1900-01-01" onChange={this.handleChange} max="2020-02-15"/>
                     </label>
+                    <br/>
                     <label>
-                    List of all Countries:
-                    <select value={this.state.dateOfBirth} onChange={this.handleChange}></select>
+                    Country:
+                        <select name = "country" value={this.state.country} onChange={this.handleChange}>
+                            {listOfCountries.map(country => {
+                                return <option name={country.name} key ={country.name}>{country.name}</option>
+                            })}
+                        </select>
+                    </label>
+                    <br/>
+                    <label>
+                        Dietary Preference: 
+                        <select name="dietaryOptions" value={this.state.dietaryOptions} onChange={this.handleChange}>
+                            <option value="Omnivore">Omnivore</option>
+                            <option value="Vegitarian">Vegitarian</option>
+                            <option value="Vegan">Vegan</option>
+                        </select>
+                    </label>
+                    <br/>
+                    <label>
+                        Why Do You Want to be a Mars Explorer?: 
+                        <input type="text" name="question" value={this.state.question} onChange={this.handleChange}/>
                     </label>
                 </form>
             </div>
