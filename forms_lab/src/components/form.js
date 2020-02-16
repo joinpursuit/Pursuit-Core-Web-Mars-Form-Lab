3b615d7d95ed95620a56e749df7c2f82b28c504f
Ohidurr <Ohidurrahman96@gmail.com>
2020-02-16 17:06:41 -0500

    complete radio button questions

--- forms_lab/src/components/form.js
+++ forms_lab/src/components/form.js
@@ -8,15 +8,20 @@ class Form extends React.Component{
         DOB:"",
         coo:"",
         diet:"omnivore",
-        reason:""
+        reason:"",
+        breathe: "Yes",
+        status: "unmarried",
+        react: "Determination",
+        claustrophobic: "No",
+        health: false,
     }
     handleChange=(e)=>{
         let results=document.querySelector("#results")
         results.innerHTML=""
-        console.log(this.state)
         this.setState({
             [e.target.name]:e.target.value
         })
+        console.log(this.state)
     }
 
     handleCompletedForm=(e)=>{
@@ -60,17 +65,20 @@ class Form extends React.Component{
     
 
     render(){
-        let {diet} = this.state
+        let { diet, breathe, status, react, claustrophobic, health } = this.state
         return(
             <form id="reg" onSubmit={this.handleSubmit}>
+
                 <label >
                     First Name:
                     <input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange}/>
                 </label>
+
                 <label>
                     Last Name:
                     <input type="text" name="lastName"  value={this.state.lastName} onChange={this.handleChange}/>
                 </label>
+
                 <label>
                     Date of Birth
                     <input type="date" name="DOB" value={this.state.DOB} onChange={this.handleChange} min="1900-01-01"/>
@@ -87,24 +95,83 @@ class Form extends React.Component{
                     Dietary Preference:
                     <label>
                     omnivore
-                    <input type="radio" name="diet" value="omnivore" checked={diet === "omnivore"} onChange={this.handleChange}/>
+                        <input type="radio" name="diet" value="omnivore" checked={diet === "omnivore"} onChange={this.handleChange}/>
                     </label>
                     <label>
                     vegetarian
-                    <input type="radio" name="diet" value="vegetarian" checked={diet === "vegetarian"} onChange={this.handleChange} />    
+                        <input type="radio" name="diet" value="vegetarian" checked={diet === "vegetarian"} onChange={this.handleChange} />    
                     </label>
                     <label>          
                     vegan    
-                    <input type="radio" name="diet" value="vegan" checked={diet === "vegan"} onChange={this.handleChange}/>
+                        <input type="radio" name="diet" value="vegan" checked={diet === "vegan"} onChange={this.handleChange}/>
                     </label>
                 </label>
+
                 <label>
                     Why do you want to be a Mars explorer
                     <input type="text" name="reason" onChange={this.handleChange} size="100"/>
                 </label>
+
                 <label>
                     <input type="submit" value="submit"/>
                 </label>
+
+                <label> 
+
+                Can you breathe underwater longer than 1 minute?
+                </label>
+
+                <label>
+                    Yes
+                    <input type="radio" name="breathe" value="Yes" checked={breathe === "Yes"} onChange={this.handleChange}/>
+                </label>
+                <label>
+                    No
+                    <input type="radio" name="breathe" value="No" checked={breathe === "No"} onChange={this.handleChange}/>
+                </label>
+                <label>
+                    I Don't Know
+                    <input type="radio" name="breathe" value="I Don't Know" checked={breathe === "I Don't Know"} onChange={this.handleChange}/>
+                </label>
+            <label>
+                What is your marital status? 
+                Married
+                <input type="radio" name="status" value="Married" checked={status === "Married"} onChange={this.handleChange}/>
+                Unmarried
+                <input type="radio" name="status" value="Unmarried" checked={status === "Unmarried"} onChange={this.handleChange}/>
+            </label>
+        <label>
+            When you are in a stressful or difficult situation, how do you most frequently react?
+
+            Determination: I continue to confront the situation.
+            <input type="radio" name="react" value="Determination" checked={react === "Determination"} onChange={this.handleChange}/>
+            
+            Defeat: I stop confronting the situation.
+            <input type="radio" name="react" value="Defeat" checked={react === "Defeat"} onChange={this.handleChange}/>
+            
+            Anger: I become upset at the situation.
+            <input type="radio" name="react" value="Anger" checked={react === "Anger"} onChange={this.handleChange}/>
+            
+            Resourcefulness: I seek help to confront the situation.
+            <input type="radio" name="react" value="Resourcefulness" checked={react === "Resourcefulness"} onChange={this.handleChange}/>
+        </label> 
+        <label>
+            Are you claustrophobic?
+
+            Yes:
+            <input type="radio" name="claustrophobic" value="Yes" checked={claustrophobic === "Yes"} onChange={this.handleChange}/>
+
+            No:
+            <input type="radio" name="claustrophobic" value="No" checked={claustrophobic === "No"} onChange={this.handleChange}/>
+
+            I Don't Know:
+            <input type="radio" name="claustrophobic" value="I don't know" checked={claustrophobic === "I don't know"} onChange={this.handleChange}/>
+         </label>
+         <label>
+            Does your family have a history of (check all that apply):
+
+           Cancer <input type="checkbox" name="health" value="Cancer" checked={health === true} onChange={this.handleChange}/>
+         </label>
             </form>
         )
     }
