38adafcea4e5321137e6e3339dda5a928c52cd4f
crystal cardona <crystalcardona@pursuit.org>
2020-02-16 12:36:37 -0500

    step 1 create form.js

---
+++ mars-form-app/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { render } from 'react-dom';
+
+class Form extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h1>Mission To Mars Registration Form</h1>
+            </div>
+        )
+    }
+}
+
+
+export default Form;
