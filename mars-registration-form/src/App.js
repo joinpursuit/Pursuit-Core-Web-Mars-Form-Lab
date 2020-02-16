d5fa728230019174c85cd56554ace446b384ae0a
Phillip <phil.awich@gmail.com>
2020-02-16 12:17:30 -0500

    Add components folder / app.js

--- mars-registration-form/src/App.js
+++ mars-registration-form/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import './App.css';
+import Form from './Components/Form.js'
 
 function App() {
   return (
     <div className="App">
-  
+      <Form/>
     </div>
   );
 }
