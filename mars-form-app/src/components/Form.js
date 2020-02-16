5f3c0cfb10a99a6ab773241d2af275b2d3796c5f
crystal cardona <crystalcardona@pursuit.org>
2020-02-16 12:58:01 -0500

    created inputs

--- mars-form-app/src/components/Form.js
+++ mars-form-app/src/components/Form.js
@@ -7,6 +7,35 @@ class Form extends React.Component {
         return(
             <div>
                 <h1>Mission To Mars Registration Form</h1>
+                <form>
+                <label>
+                What is your name?
+                <input type="text" value="name" />
+                </label>
+                <label>
+                What is your date of birth?
+                <input type="date" name="date" min="1900-01-01" />
+                </label>
+                <label>
+                What is your country of origin? 
+                <select>
+                    <option value="countries">Countries</option>
+                </select>
+                </label>
+                <label>
+                What is your dietary preference?
+                <select>
+                    <option value="omnivore">Omnivore</option>
+                    <option value="vegetarian">Vegetarian</option>
+                    <option value="vegan">Vegan</option>
+                </select>
+                </label>
+                <label>
+                Why do you want to be a Mars explorer? 
+                <textarea rows="10" cols="80" />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
             </div>
         )
     }
