53302cb16eb350c5edf79bdd241972ef5ee3f213
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-02-16 12:59:01 -0500

    tried to debug populating country

--- mars_form_app/src/components/Form.js
+++ mars_form_app/src/components/Form.js
@@ -1,20 +1,67 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class Form extends Component{
     state = {
-        
-    };
+        countryList: []
+    }
+
+    populateCountry(e) {
+        e.preventDefault();
+        axios.get("http://vocab.nic.in/rest.php/country/json").then(res => {
+            const countryList = res.data;
+            this.setState({ countryList });
+        })
+    }
+
+    
     render(){
+        console.log(this.state);
         return(
             <div>
                 <form>
                     <h1>
                         Mission to Mars Registration Form
                     </h1>
+                    <label>
+                        Name:
+                        <input type="text" placeholder="What's your name?"></input>
+                    </label>
+                    <br></br>
+                    <label>
+                        Date of Birth:
+                        <input type="date"></input>
+                    </label>
+                    <br></br>
+                    <label>
+                        Country of Origin:
+                        <select id="countryList">
+                            <option disabled>Select Country</option>
+                            { this.state.countryList.map(country => <option>{country}</option>) }
+                        </select>
+                    </label>
+                    <br></br>
+                    <label>
+                        Dietary Preference:
+                        <select >
+                            <option disabled>Select Dietary Preference</option>
+                            <option>Omnivore</option>
+                            <option></option>
+                        </select>
+                    </label>
                 </form>
             </div>
         )
     }
+
+    
+    
+          
+        
+    
+
+    
+
 }
 
 export default Form;
